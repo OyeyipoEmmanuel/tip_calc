baebf61523f924a82eb6fd7e53e544157cdc4570
Emmanuel <emmasco0207@gmail.com>
2026-06-03 04:30:42 +0100

    added logic

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,18 @@
 import { Stack } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { StyleSheet } from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaProvider style={styles.root}>
+      <Stack />
+    </SafeAreaProvider>
+  );
 }
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    fontFamily: 'System', // uses iOS San Francisco / Android Roboto
+  },
+});
